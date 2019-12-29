d9294f17ea6a3e7b1d21e7bb275af46665c59d1d
Afonso -aptsharp <aff-on@hotmail.com>
2019-12-29 20:13:23 -0300

    StreamWriter - Imprementação do metodo de leitura - Metodo otimizado

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Manip.Arq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\aff-o\source\repos\Manip.Arq\Msg.txt";
+            string targetPath = @"C:\Users\aff-o\source\repos\Manip.Arq\Msg0.txt";
+
+            Console.WriteLine(sourcePath);
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                //append é uma palavra de concatenação (acrescenta no final do arquivo o que for escrito)
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine(e.Message);
+
+            }
+        }
+    }
+}
